13e3c6d9e4664571a521b7841269ee9952f5fea1
Federico Cabrera <fcabrera.all@gmail.com>
2022-07-20 11:07:37 -0300

    Create getQuotation() function

---
+++ signin/scraper.py
@@ -0,0 +1,33 @@
+import requests, os
+import pandas as pd
+from bs4 import BeautifulSoup as bs4
+from dotenv import load_dotenv
+
+# SENSIBLE
+load_dotenv()
+HEADER = {"User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+URL = os.getenv("URL")
+
+def getQuotation(url=URL, header=HEADER):
+    page = requests.get(url, headers=header)
+    soup = bs4(page.content, "lxml")
+    
+    get_quotation_usd = soup.find("table", {"class" : "table cotizacion"})
+    get_quotation_date = soup.find("th", {"class" : "fechaCot"})
+    get_quotation_time = soup.find("div", {"class" : "legal"})
+
+    for row in get_quotation_usd.tbody.find_all('tr'):    
+        columns = row.find_all('td')
+        divisa = columns[0].text.strip()
+        cotizacion_compra = columns[1].text.strip()
+        cotizacion_venta = columns[2].text.strip()
+        data = {
+            "DIVISA": [divisa],
+            "COMPRA": [cotizacion_compra],
+            "VENTA" : [cotizacion_venta],
+            "FECHA" : [get_quotation_date.text],
+            "HORA" : [get_quotation_time.text[20:]]
+        }
+        df = pd.DataFrame(data, index=["1"])
+        return df
+
