c4ec935171b4ce59fed9c91fb239d47337a4a93f
Federico Cabrera <fcabrera.all@gmail.com>
2022-07-21 18:06:30 -0300

    Create test plot

--- signin/routes.py
+++ signin/routes.py
@@ -1,19 +1,42 @@
+import io, base64
 from signin import app, db
 from signin.models import User
 from signin.forms import RegisterForm, LoginForm, ResetPasswordForm
 from signin.scraper import getQuotation
 from flask_login import login_user
 from flask import render_template, flash, redirect, url_for, request
-
-
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 # Routes
 @app.route("/")
-@app.route("/python-flask-signin/home.html")
+@app.route("/python-flask-signin/home.html", methods=["GET"])
 def home_page():
     if request.method == "GET":
         df = getQuotation()
-    return render_template("/home.html", tables=[df.to_html(classes='data', header=True, index=False, bold_rows=True)])
+        
+        # Generate plot
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        axis.set_title("U$D")
+        axis.set_xlabel("Date")
+        axis.set_ylabel("Value")
+        axis.grid()
+        axis.plot(range(130,330,10), "ro-")
+    
+        # Convert plot to PNG image
+        pngImage = io.BytesIO()
+        FigureCanvas(fig).print_png(pngImage)
+
+        # Encode PNG image to base64 string
+        pngImageB64String = "data:image/png;base64,"
+        pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+    return render_template( "/home.html", 
+                            column_names=df.columns.values, 
+                            row_data=list(df.values.tolist()),
+                            zip=zip, image=pngImageB64String
+                          )
 
 @app.route("/python-flask-signin/register.html", methods=["GET", "POST"])
 def register_page():
